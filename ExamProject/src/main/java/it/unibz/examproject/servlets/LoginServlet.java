529a4f747abb779dbf5da1edf23635239fc7281c
Riccardo Rigoni <rrigoni@unibz.it>
2022-04-15 18:42:28 +0200

    Update: dynamic load of configuration properties

--- ExamProject/src/main/java/it/unibz/examproject/servlets/LoginServlet.java
+++ ExamProject/src/main/java/it/unibz/examproject/servlets/LoginServlet.java
@@ -20,7 +20,8 @@ public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private static Connection conn;
-	
+
+
 	/**
      * @see HttpServlet#HttpServlet()
      */
