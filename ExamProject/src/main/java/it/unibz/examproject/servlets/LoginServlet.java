6be0810a584bcb9e1243f86706ab3473510145b3
Riccardo Rigoni <rrigoni@unibz.it>
2022-04-25 15:04:45 +0200

    Update:

--- ExamProject/src/main/java/it/unibz/examproject/servlets/LoginServlet.java
+++ ExamProject/src/main/java/it/unibz/examproject/servlets/LoginServlet.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServlet;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
